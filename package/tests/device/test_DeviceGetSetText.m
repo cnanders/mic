65db4ac8f23f1a4a0d2da5ff1bc80ec923fb9571
metmatlab <metmatlab>
2017-03-02 14:29:34 -0800

    removing package which is now at cnanders/github/matlab-instrument-control

--- package/tests/device/test_DeviceGetSetText.m
+++
@@ -1,9 +0,0 @@
-[cDirThis, cName, cExt] = fileparts(mfilename('fullpath'));
-cDirMic = fullfile(cDirThis, '..', '..');
-addpath(genpath(cDirMic));
-
-purge
-
-device = mic.device.GetSetText();
-device.set('car')
-device.get()
